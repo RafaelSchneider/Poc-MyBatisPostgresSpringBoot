1b3a48823861d1eca86669944fe2b28837e13166
Rafael Schneider <rss.schneider@gmail.com>
2017-11-16 22:55:55 -0200

    ajustes

--- springrest-service-impl/src/main/java/br/com/rest/springrest/database/PessoaServiceDao.java
+++ springrest-service-impl/src/main/java/br/com/rest/springrest/database/PessoaServiceDao.java
@@ -1,8 +1,11 @@
 package br.com.rest.springrest.database;
 
+import br.com.rest.springrest.database.mapper.PessoaMapper;
 import br.com.rest.springrest.model.PessoaModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.sql.SQLException;
 
 /**
  * Created by schneider on 16/11/17.
@@ -12,13 +15,15 @@ public class PessoaServiceDao {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    public PessoaModel getPessoaId(Long id){
+    public PessoaModel getPessoaId(Long id) throws SQLException{
         try{
             return jdbcTemplate.queryForObject("Select id, nome, idade from pessoas "+
-                                               " where id = ? ", new   );
+                                               " where id = ? ", new PessoaMapper(), new Object[]{id} );
 
         } catch (Exception e){
-            System.out.println("Ocorreu um erro: "+e.getMessage());
+                System.out.println("Ocorreu um erro: "+e.getMessage());
+                return new PessoaModel(); //corrigir
         }
+
     }
 }
--- springrest-service-impl/src/main/java/br/com/rest/springrest/database/mapper/PessoaMapper.java
+++ springrest-service-impl/src/main/java/br/com/rest/springrest/database/mapper/PessoaMapper.java
@@ -1,7 +1,23 @@
 package br.com.rest.springrest.database.mapper;
 
+import br.com.rest.springrest.model.PessoaModel;
+
+import org.springframework.jdbc.core.RowMapper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Created by schneider on 16/11/17.
  */
-public class PessoaMapper {
+public class PessoaMapper implements RowMapper<PessoaModel> {
+
+    @Override
+    public PessoaModel mapRow(ResultSet rs, int rowNum) throws SQLException{
+        PessoaModel pessoa = new PessoaModel();
+        pessoa.setId(rs.getLong("id"));
+        pessoa.setNome(rs.getString("nome"));
+        pessoa.setIdade(rs.getInt("idade"));
+        return pessoa;
+
+    }
 }
